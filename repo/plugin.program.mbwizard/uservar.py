8f44895feb613c525570dba63060a948e5ffda98
nzmodbox <pbrobyn@hotmail.com>
2023-09-29 17:14:47 +1300

    1

---
+++ repo/plugin.program.mbwizard/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://nzmodbox.github.io/nzmodbox.com/builds.xml'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://nzmodbox.github.io/nzmodbox.com/notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.Program.mbwizard']
